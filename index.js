aee2b47fd959f70ba14057189526b4bde5bf92af
shuvo90901 <shuvo90901>
2022-10-26 14:02:05 +0600

    express added

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('API Running')
+})
+
+
+
+app.listen(port, () => {
+    console.log('Server is running on port ', port)
+})
