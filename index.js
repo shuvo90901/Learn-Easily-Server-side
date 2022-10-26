abfd8610521a5955087c07b21291a2e1bac554d2
shuvo90901 <shuvo90901>
2022-10-26 14:04:57 +0600

    categorie added

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-
+const categories = require('./data/categorie.json');
 
 
 app.get('/', (req, res) => {
     res.send('API Running')
 })
 
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
 
 
 app.listen(port, () => {
