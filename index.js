fe7a435fb8cd2bef2c599d87c346072a1bc284cd
shuvo90901 <shuvo90901>
2022-10-27 14:02:00 +0600

    vercel added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(cors());
 
 const categories = require('./data/categorie.json');
 
+const course = require('./data/course.json')
+
 
 app.get('/', (req, res) => {
     res.send('API Running')
@@ -15,6 +17,16 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/course', (req, res) => {
+    res.send(course)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const single_course = course.find(n => n.id === id);
+    res.send(single_course)
 })
 
 
